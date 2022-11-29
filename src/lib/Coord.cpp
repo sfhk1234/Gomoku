2be65622ddcdbfd7bdb8e8ae2a3eac24d438fd04
Jay Cho <jaycho1214@gmail.com>
2022-11-29 22:50:41 +0900

    add test for coord structure

---
+++ src/lib/Coord.cpp
@@ -0,0 +1,17 @@
+#include "Coord.h"
+
+Coord::Coord() {
+  /**
+   * @brief Empty constructor of coord
+   * empty constructor initalizes each point to zero
+   */
+}
+
+Coord::Coord(int x, int y) {
+  /**
+   * @brief This constructor intializes each point to given parameters
+   * @param x position of x, must be a whole number (x >= 0)
+   * @param x position of y, must be a whole number (y >= 0)
+   * @throw std::out_of_range throws error std::out_of_range with message "Each point must be whole number" if one of points has negative number
+   */
+}
