01a59cebb6c55aecbc523b54ae1ee7d199eba449
Jay Cho <jaycho1214@gmail.com>
2022-11-29 22:26:25 +0900

    Revert "add constructor on each struct"
    This reverts commit 7855299c231c32591aff8d4a9b59a1d7c33d27b8.

--- src/includes/Coord.h
+++ src/includes/Coord.h
@@ -1,8 +1,6 @@
 #pragma once
 
 struct Coord {
-  Coord(int _x, int _y): x(_x), y(_y) {}
-
   int x;
   int y;
 };
--- src/includes/Piece.h
+++ src/includes/Piece.h
@@ -4,8 +4,6 @@
 #include "Coord.h"
 
 struct Piece {
-  Piece(Coord _coord, Color _color): coord(_coord), color(_color) {}
-
   Coord coord;
   Color color;
 };
